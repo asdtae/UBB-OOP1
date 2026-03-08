629598ef2b57b8ce2d9c24658f0fab0d615c7884
asdtae <112775132+asdtae@users.noreply.github.com>
2026-03-08 23:47:57 +0200

    L1b

--- L1/L1b/mrim2553_L1_1b.cpp
+++ L1/L1b/mrim2553_L1_1b.cpp
@@ -7,13 +7,49 @@ void spiralSorrend(const int* matrix, int n, int* spiral)
 {
     int sqrtN = sqrt(n);
 
-    for (int i = 0; i < n; i++)
+    int felso = 0;
+    int also = sqrtN - 1;
+    int bal = 0;
+    int jobb = sqrtN - 1;
+
+    int k = 0;
+
+    while (felso <= also && bal <= jobb)
     {
-        
 
+        for (int i = bal; i <= jobb; i++, k++)
+        {
+            spiral[k] = matrix[felso * sqrtN + i];
+        }
 
+        felso++;
 
+        for (int i = felso; i <= also; i++, k++)
+        {
+            spiral[k] = matrix[i * sqrtN + jobb];
+        }
 
+        jobb--;
+
+        if (felso <= also)
+        {
+            for (int i = jobb; i >= bal; i--, k++)
+            {
+                spiral[k] = matrix[also * sqrtN + i];
+            }
+
+            also--;
+        }
+
+        if (bal <= jobb)
+        {
+            for (int i = also; i >= felso; i--, k++)
+            {
+                spiral[k] = matrix[i * sqrtN + bal];
+            }
+
+            bal++;
+        }
     }
 }
 
@@ -26,12 +62,12 @@ int main()
 
     int sqrtN = sqrt(n);
 
-    int prespiral[n];
+    int preSpiral[n];
     int spiral[n];
 
-    for (int i = 0; i < n; i++) cin >> prespiral[i];
+    for (int i = 0; i < n; i++) cin >> preSpiral[i];
 
-    spiralSorrend(prespiral, n, spiral);
+    spiralSorrend(preSpiral, n, spiral);
 
     for (int i = 0; i < sqrtN; i++)
     {
@@ -42,6 +78,25 @@ int main()
 
         cout << endl;
     }
+
+    /*
+        n = 25
+
+        IN
+            0  1  2  3  4
+            10 11 12 13 14
+            20 21 22 23 24
+            30 31 32 33 34
+            40 41 42 43 44
+
+        OUT
+            0 1 2 3 4
+            14 24 34 44 43
+            42 41 40 30 20
+            10 11 12 13 23
+            33 32 31 21 22
+
+    */
 
     return 0;
 }
