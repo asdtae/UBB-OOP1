7c60cf45696b6f7ff52d904cdecc58903155d812
asdtae <112775132+asdtae@users.noreply.github.com>
2026-03-18 16:22:12 +0200

    l2

---
+++ Lab2/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Mathe Ruben-Jonathan
+ *  mrim2553
+ *  512/2
+ *
+ *  Lab2
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ labor/02/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *  Mathe Ruben-Jonathan
+ *  mrim2553
+ *  512/2
+ *  feladat_nev
+ */
+#include <iostream>
+
+using namespace std;
+
+class Matrix
+{
+
+};
+
+int main()
+{
+
+    return 0;
+}
